1f8b5061914b1b8f1864adb28200f70a7aab7819
Changhyeok Bae <changhyeok.bae@gmail.com>
2018-08-21 06:16:55 +0000

    webos-image: Update layer pins for 2018-09-20 (build webosce_1)
    meta-webosose as of 2018-09-20 14:32:14 +0000
    
    1eecf4e qemu: support graphic hw acceleration
    a58e57e Enable libsdl2 component
    67f8784 webos-repositories.inc: Change repo address to github.com/webosce

--- weboslayers.py
+++ weboslayers.py
@@ -70,16 +70,16 @@ webos_layers = [
 
 ('meta-qt5',                  20, 'git://github.com/meta-qt5/meta-qt5.git',                 'branch=krogoth,commit=dcfcb58', ''),
 
-('meta-webos-backports-2.3',  30, 'git://github.com/webosose/meta-webosose.git',            '', ''),
-('meta-webos-backports-2.4',  31, 'git://github.com/webosose/meta-webosose.git',            '', ''),
-('meta-webos-backports-2.5',  32, 'git://github.com/webosose/meta-webosose.git',            '', ''),
-('meta-webos-backports-2.6',  33, 'git://github.com/webosose/meta-webosose.git',            '', ''),
+('meta-webos-backports-2.3',  30, 'git://github.com/webosce/meta-webosose.git',             '', ''),
+('meta-webos-backports-2.4',  31, 'git://github.com/webosce/meta-webosose.git',             '', ''),
+('meta-webos-backports-2.5',  32, 'git://github.com/webosce/meta-webosose.git',             '', ''),
+('meta-webos-backports-2.6',  33, 'git://github.com/webosce/meta-webosose.git',             '', ''),
 
 ('meta-ros2',                 38, 'git://github.com/lgsvl/meta-ros2.git',                   'branch=ros2pr,commit=fac79da',''),
-('meta-webos',                40, 'git://github.com/webosose/meta-webosose.git',            'branch=master,commit=8699f9b', ''),
+('meta-webos',                40, 'git://github.com/webosce/meta-webosose.git',             'branch=webosce,commit=1eecf4e', ''),
 
 ('meta-raspberrypi',          50, 'git://git.yoctoproject.org/meta-raspberrypi',            'branch=morty,commit=2a19226', ''),
-('meta-webos-raspberrypi',    51, 'git://github.com/webosose/meta-webosose.git',            '', ''),
+('meta-webos-raspberrypi',    51, 'git://github.com/webosce/meta-webosose.git',             '', ''),
 
-('meta-webos-ros2',           52, 'git://github.com/webosose/meta-webosose.git',            '', ''),
+('meta-webos-ros2',           52, 'git://github.com/webosce/meta-webosose.git',             '', ''),
 ]
